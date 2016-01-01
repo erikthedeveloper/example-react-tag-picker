78012eb3844704c248dc12978f8f3644e435c68e
Erik Aybar <erikthedeveloper@gmail.com>
2016-01-01 07:46:57 -0700

    Install/Config webpack + package.json commands [build, build:watch]

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // The entry point for our application
+  entry: './src/index.js',
+  // The directory and filename for our bundled javascript
+  output: {
+    path: './dist',
+    filename: 'bundle.js'
+  }
+};
